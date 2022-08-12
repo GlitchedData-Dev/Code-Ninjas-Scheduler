2d47b17a745031a9d290fe73b8eaffc5969eea23
GlitchedData <jojoman1703@gmail.com>
2022-08-13 00:48:45 +0100

    Add project files.

---
+++ Code Ninjas Scheduler/MVVM/View/SettingsView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Code_Ninjas_Scheduler.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for SettingsView.xaml
+    /// </summary>
+    public partial class SettingsView : UserControl
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+            //Make sure runtime settings are up to date
+            userSettings.Default.Upgrade();
+            UpdateTextbox();
+        }
+
+        //Set text in settings textboxes to stored settings
+        public void UpdateTextbox()
+        {
+            emailField.Text = userSettings.Default.SenderEmail.ToString();
+            passwordField.Text = userSettings.Default.SenderPassword.ToString();
+            nameField.Text = userSettings.Default.SenderName.ToString();
+            recipientEmailField.Text = userSettings.Default.RecipientEmail.ToString();
+        }
+
+        //Called when submit button in the settings view is pressed
+        //Saves new settings based on text in textboxes
+        public void ApplySettings(object sender, RoutedEventArgs e)
+        {
+            userSettings.Default.SenderEmail = emailField.Text;
+            userSettings.Default.SenderName = nameField.Text;
+            userSettings.Default.SenderPassword = passwordField.Text;
+            userSettings.Default.RecipientEmail = recipientEmailField.Text;
+            userSettings.Default.Save();
+            userSettings.Default.Reload();
+            userSettings.Default.Upgrade();
+            UpdateTextbox();
+        }
+
+        //Called when cancel button in the settings view is pressed
+        //Resets all settings
+        public void ClearSettings(object sender, RoutedEventArgs e)
+        {
+            userSettings.Default.Reset();
+            UpdateTextbox();
+        }
+    }
+}
---
+++ Code Ninjas Scheduler/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Code_Ninjas_Scheduler.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Ninjas_Scheduler.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand SchduleVMCommand { get; set; }
+        public RelayCommand SettingsVMCommand { get; set; }
+        public RelayCommand MenuToggleCommand { get; set; }
+
+        public ScheduleViewModel ScheduleVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            ScheduleVM = new ScheduleViewModel();
+            SettingsVM = new SettingsViewModel();
+            CurrentView = ScheduleVM;
+
+            SchduleVMCommand = new RelayCommand(o =>
+            {
+                CurrentView = ScheduleVM;
+            });
+
+            SettingsVMCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVM;
+            });
+
+            MenuToggleCommand = new RelayCommand(o =>
+            {
+                if (CurrentView == SettingsVM)
+                {
+                    CurrentView = ScheduleVM;
+                }
+                else
+                {
+                    CurrentView = SettingsVM;
+                }
+            });
+        }
+    }
+}
